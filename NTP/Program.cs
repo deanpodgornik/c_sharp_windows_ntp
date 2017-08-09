fc3d6d9cdce81c71092f5eb7c4d1fe35b5751cd4
Dean Podgornik <dean.podgornik@gmail.com>
2017-08-09 17:47:39 +0200

    setting date and added config file

--- NTP/Program.cs
+++ NTP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuerrillaNtp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace NTP
 {
     class Program
     {
+        /// <summary>
+        /// Set date of the system
+        /// </summary>
+        /// <param name="dateInYourSystemFormat">Format: dd-mm-yy</param>
         static void setDate(string dateInYourSystemFormat)
         {
             var proc = new System.Diagnostics.ProcessStartInfo();
@@ -26,6 +31,11 @@ namespace NTP
 
             }
         }
+
+        /// <summary>
+        /// Set time of the system
+        /// </summary>
+        /// <param name="timeInYourSystemFormat">Format: mm:ss</param>
         static void setTime(string timeInYourSystemFormat)
         {
             var proc = new System.Diagnostics.ProcessStartInfo();
@@ -45,10 +55,37 @@ namespace NTP
             }
         }
 
+        /// <summary>
+        /// Retrieve the current time from a NTP service
+        /// </summary>
+        static DateTime retrieveCurrentTime() {
+            //setTime("10:10");
+            //setDate("01.02.2017");
+
+            // perform only one query to get the offset
+            TimeSpan offset;
+            using (var ntp = new NtpClient(Settings.Default.NTPServer))
+            {
+                ntp.Timeout = TimeSpan.FromSeconds(5);
+                var ntpResponse = ntp.Query();
+                offset = ntpResponse.CorrectionOffset;
+            }
+            // use the offset throughout your app
+            return DateTime.UtcNow + offset;
+        }
+
         static void Main(string[] args)
         {
-            setTime("10:10");
-            setDate("01.02.2017");
+            
+            //TODO: register as service
+            
+            //TODO: implement retry in case of no internet connection
+            
+            var currentTime = retrieveCurrentTime();
+            //setting time
+            setTime(currentTime.Hour+":"+currentTime.Minute);
+            //setting date
+            setDate(currentTime.Day+"-"+currentTime.Month+"-"+currentTime.Year);
         }
     }
 }
